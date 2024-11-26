39ed43fb73d83c42f8d29caeaaf961320509b1b5
jungseok <jungsoeko@gmail.com>
2024-11-26 14:43:34 +0900

    11.26

---
+++ React/travel-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PhotoDetail from './PhotoDetail/PhotoDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <PhotoDetail />
+    </div>
+  );
+}
+
+export default App;
